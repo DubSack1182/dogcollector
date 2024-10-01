3943a626d3bbb9f677559834f4b711f36131fe02
kevindbln@gmail.com <kevindbln@gmail.com>
2024-10-01 14:07:34 -0400

    Completed Lab 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,18 +1,5 @@
 from django.shortcuts import render
-
-class Dog:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-dogs = [
-    Dog('Lolo', 'tabby', 'Kinda rude.', 3),
-    Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Dog('Bonk', 'selkirk rex', 'Barks loudly.', 6)
-]
+from .models import Dog
         
 
 # Create your views here.
@@ -23,4 +10,9 @@ def about(request):
     return render(request, 'about.html')
 
 def dog_index(request):
+    dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dog_detail(request, dog_id):
+    dogs = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dogs': dogs })
