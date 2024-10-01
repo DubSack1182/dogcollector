5f5a30f6633a1902aa3337a745b36afdf4bc6f4d
kevindbln@gmail.com <kevindbln@gmail.com>
2024-10-01 08:59:10 -0400

    FinchCollector Part 2 Complete

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
+class Dog:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age
+
+dogs = [
+    Dog('Lolo', 'tabby', 'Kinda rude.', 3),
+    Dog('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
+    Dog('Fancy', 'bombay', 'Happy fluff ball.', 4),
+    Dog('Bonk', 'selkirk rex', 'Barks loudly.', 6)
+]
+        
+
+# Create your views here.
 def home(request):
-    return HttpResponse('<h1>Hello Dog Collector</h1>')
+    return render(request, 'home.html')
 
 def about(request):
-    return HttpResponse('<h1>About the Dog Collector</h1>')
+    return render(request, 'about.html')
+
+def dog_index(request):
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
